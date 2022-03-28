61dc9e9cb0ee2b0026146869e4305101c6eb34e3
michio <80969068+michioxd@users.noreply.github.com>
2022-03-28 22:56:23 +0700

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,20 @@
+firewall-cmd --zone=public --add-port=2222/tcp --permanent
+firewall-cmd --zone=public --add-port=21/tcp --permanent
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --zone=public --add-port=25/tcp --permanent
+firewall-cmd --reload
+systemctl restart directadmin
+cd /usr/local/directadmin/conf/
+service directadmin stop
+rm -rf /usr/local/directadmin/conf/license.key
+wget -O /usr/local/directadmin/conf/license.key https://raw.githubusercontent.com/puarudz/DirectAdmin-1.59.5/master/license.key
+chmod 600 /usr/local/directadmin/conf/license.key
+chown diradmin:diradmin /usr/local/directadmin/conf/license.key
+ifconfig eth0:100 103.127.207.212 netmask 255.0.0.0 up
+echo 'DEVICE=eth0:100' >> /etc/sysconfig/network-scripts/ifcfg-eth0:100
+echo 'IPADDR=103.127.207.212' >> /etc/sysconfig/network-scripts/ifcfg-eth0:100
+echo 'NETMASK=255.0.0.0' >> /etc/sysconfig/network-scripts/ifcfg-eth0:100
+service network restart
+/usr/bin/perl -pi -e 's/^ethernet_dev=.*/ethernet_dev=eth0:100/' /usr/local/directadmin/conf/directadmin.conf
+service directadmin start
+ifdown eth0:100
